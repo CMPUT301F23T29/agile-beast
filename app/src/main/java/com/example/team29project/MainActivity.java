6d4c5679e3f603604e671ac2f8dfe2228fe6fd83
mjeong@ualberta.ca <mjeong@ualberta.ca>
2023-11-06 00:52:21 -0700

    InputFragment initialized but errors in opening dialog

--- app/src/main/java/com/example/team29project/MainActivity.java
+++ app/src/main/java/com/example/team29project/MainActivity.java
@@ -3,7 +3,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -16,18 +18,21 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.SlidingDrawer;
+import android.widget.TextView;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Button camera ;
+    TextView addItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        ImageButton menu = (ImageButton)findViewById(R.id.menu);
+        ImageButton menu =findViewById(R.id.menu);
 //        ConstraintLayout menuBackgroundLayout = (ConstraintLayout) findViewById(R.id.menu_background_layout);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,18 +46,22 @@ public class MainActivity extends AppCompatActivity {
     public void popupMenu(View view){
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.fragment_main_menu, null);
-
         int height = LinearLayout.LayoutParams.MATCH_PARENT;
         boolean focusable = true;
         final PopupWindow popupWindow = new PopupWindow(popupView, 750, height, focusable);
         popupWindow.showAtLocation(view, Gravity.LEFT, 0, 0);
-
-        popupView.setOnTouchListener(new View.OnTouchListener() {
+        addItem = popupView.findViewById(R.id.add_new_item);
+        addItem.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
-                return true;
+            public void onClick(View v) {
+                    new InputFragment().show(getSupportFragmentManager(), "addItems");
             }
         });
+
+
+      popupView.setOnTouchListener((v, event) -> {
+                    popupWindow.dismiss();
+                    return true;
+                });
     }
 }
