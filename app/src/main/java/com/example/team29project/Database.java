560e1d329cdda7a12c85215d947887387382cb7c
Jacob Feng <peanutChowder@gmail.com>
2023-11-09 10:57:03 -0700

    Implemented db adder and item initial loder

--- app/src/main/java/com/example/team29project/Database.java
+++ app/src/main/java/com/example/team29project/Database.java
@@ -1,12 +1,94 @@
 package com.example.team29project;
 
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Database {
     private FirebaseFirestore db;
     private CollectionReference itemsRef;
-    public Database() {
+    private ArrayList<Item> dataList;
+    private ItemArrayAdapter itemAdapter;
 
+    public Database(ItemArrayAdapter itemAdapter) {
+        this.itemAdapter = itemAdapter;
+
+        db = FirebaseFirestore.getInstance();
+        itemsRef = db.collection("items");
+
+        // Load the items from FireBase remote into dataList and display them
+        loadInitialItems();
     }
+
+    private void loadInitialItems() {
+        // Add a snapshot listener to the Firestore reference 'itemsRef'
+        itemsRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                // If there's an error with the snapshot, log the error
+                if (error != null) {
+                    Log.e("Firebase", error.toString());
+                }
+
+                // If the snapshot is not null (i.e., there's data at 'itemsRef')
+                if (value != null) {
+                    // Clear the 'dataList'
+                    dataList.clear();
+
+                    // Loop over each document in the snapshot
+                    for (QueryDocumentSnapshot doc : value) {
+                        // Retrieve various fields from the document
+                        String name = doc.getId();
+                        String date = doc.getString("date");
+                        double itemValue = Double.parseDouble(Objects.requireNonNull(doc.getString("value")));
+                        String make = doc.getString("make");
+                        String model = doc.getString("model");
+                        String serialNumber = doc.getString("serialNumber");
+                        String description = doc.getString("description");
+                        String comment = doc.getString("comment");
+
+                        // Add a new 'Item' object to 'dataList' with these fields
+                        dataList.add(new Item(name, date, itemValue, make, model, description, comment, serialNumber));
+                    }
+
+                    // refresh ListView and display the new data
+                    itemAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    public void addItem(Item item) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("date", item.getDate());
+        data.put("value", item.getValue().toString());
+        data.put("make", item.getMake());
+        data.put("model", item.getModel());
+        data.put("serialNumber", item.getSerialNumber());
+        data.put("description", item.getDescription());
+        data.put("comment", item.getComment());
+        // Add the 'data' map to the Firestore database under a document named after the item's name.
+        itemsRef
+                .document(item.getName())
+                .set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("Firestore", "Document snapshot written successfully!");
+                    }
+                });
+    }
+
 }
