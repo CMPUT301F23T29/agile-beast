1a94229006599f004b90f2d5f891aabaeab16207
Jacob Feng <peanutChowder@gmail.com>
2023-11-08 17:19:55 -0700

    New class for Firebase related operations

---
+++ app/src/main/java/com/example/team29project/Database.java
@@ -0,0 +1,12 @@
+package com.example.team29project;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Database {
+    private FirebaseFirestore db;
+    private CollectionReference itemsRef;
+    public Database() {
+
+    }
+}
