26b5b7df486cdc9f172e0c38076a1bbab37e9861
mjeong@ualberta.ca <mjeong@ualberta.ca>
2023-12-01 14:03:31 -0700

    Changed the barcode api since the older one is corrupted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,10 +53,9 @@ dependencies {
     implementation("androidx.camera:camera-core:1.2.1")
     implementation("androidx.camera:camera-lifecycle:1.2.1")
     implementation("androidx.camera:camera-view:1.2.1")
-    implementation ("com.google.android.gms:play-services-code-scanner:16.1.0")
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
     implementation ("com.journeyapps:zxing-android-embedded:4.2.0")
     implementation ("com.google.mlkit:text-recognition:16.0.0")
     implementation ("org.mindrot:jbcrypt:0.4")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-    compileOnly(files("${android.sdkDirectory}/platforms/${android.compileSdkVersion}/android.jar"))
 }
