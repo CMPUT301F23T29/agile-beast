328b4f6252c4c912c7e9bb1aff9397ea38128e44
Jacob Feng <peanutChowder@gmail.com>
2023-12-03 14:34:29 -0700

    Added db getter

--- app/src/main/java/com/example/team29project/View/MainPageActivity.java
+++ app/src/main/java/com/example/team29project/View/MainPageActivity.java
@@ -155,6 +155,10 @@ public class MainPageActivity extends AppCompatActivity implements
 
     }
 
+    public DatabaseController getDb() {
+        return db;
+    }
+
     /**
      * Update the summary of value of item
      */
