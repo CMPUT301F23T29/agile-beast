870f9884b74569919523ca8c6677fec03c6fa5c3
Jacob Feng <peanutChowder@gmail.com>
2023-12-02 20:20:44 -0700

    Added test for tag button opening taglist dialog

--- app/src/androidTest/java/com/example/team29project/MenuTest.java
+++ app/src/androidTest/java/com/example/team29project/MenuTest.java
@@ -2,26 +2,28 @@ package com.example.team29project;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.anything;
 import static org.junit.Assert.assertTrue;
 
+import android.content.Intent;
+
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
 
-import com.example.team29project.Model.Item;
+import com.example.team29project.View.ItemViewActivity;
 import com.example.team29project.View.MainActivity;
+import com.example.team29project.View.MainPageActivity;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,19 +33,18 @@ import java.lang.reflect.Field;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MenuTest {
-    private MainActivity getActivityInstance(ActivityScenarioRule<MainActivity> scenarioRule) {
-        final MainActivity[] activity = new MainActivity[1];
-        scenarioRule.getScenario().onActivity(new ActivityScenario.ActivityAction<MainActivity>() {
-            @Override
-            public void perform(MainActivity mainActivity) {
-                activity[0] = mainActivity;
-            }
-        });
-        return activity[0];
-    }
     @Rule
-    public ActivityScenarioRule<MainActivity> scenario=
-            new ActivityScenarioRule<MainActivity>(MainActivity.class);
+    public ActivityTestRule<MainPageActivity> mainPageActivityActivityTestRule =
+            new ActivityTestRule<>(MainPageActivity.class, false, false);
+
+    @Before
+    public void setUp() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), MainPageActivity.class);
+        intent.putExtra("userId", "a");
+
+        mainPageActivityActivityTestRule.launchActivity(intent);
+    }
+
     @Test
     public void openMenuTest() {
         // Open menu
@@ -59,12 +60,11 @@ public class MenuTest {
     }
 
     @Test
-    public void editTagButtonTest() throws Exception {
+    public void tagButtonLaunchesTagListTest() {
         onView(withId(R.id.menu)).perform(click());
         onView(withId(R.id.edit_tag_item)).perform(click());
 
-        // TODO: Wait until edit tag is implemented
-        throw new Exception();
+        onView(withText("Delete")).check(matches(isDisplayed()));
     }
 
     @Test
@@ -81,11 +81,6 @@ public class MenuTest {
                 .atPosition(0)
                 .perform(click());
 
-        MainActivity mainActivity = getActivityInstance(scenario);
-        Field isSelectField = MainActivity.class.getDeclaredField("isSelect");
-        isSelectField.setAccessible(true);
-        boolean isSelectValue = (boolean) isSelectField.get(mainActivity);
-        assertTrue(isSelectValue);
     }
 
 }
