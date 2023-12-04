703bbde6248c985081bd29432dbe950397defce0
Jacob Feng <peanutChowder@gmail.com>
2023-12-04 13:10:58 -0700

    Added removal tests

--- app/src/androidTest/java/com/example/team29project/FireBaseControllerTest.java
+++ app/src/androidTest/java/com/example/team29project/FireBaseControllerTest.java
@@ -32,6 +32,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -40,6 +41,7 @@ import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -50,6 +52,8 @@ public class FireBaseControllerTest {
     private StorageReference imgRef;
     private CollectionReference itemsRef;
     private CollectionReference tagsRef;
+    private ArrayList<Item> itemDataList;
+    private ArrayList<Tag> tagDataList;
 
 
 
@@ -60,6 +64,8 @@ public class FireBaseControllerTest {
     public void setUp() {
         db = mock(FirebaseFirestore.class, Mockito.RETURNS_DEEP_STUBS);
         sb = mock(FirebaseStorage.class);
+        itemDataList = spy(ArrayList.class);
+        tagDataList = spy(ArrayList.class);
 
         // Set up return values for init
         imgRef = mock(StorageReference.class);
@@ -70,7 +76,7 @@ public class FireBaseControllerTest {
         Mockito.when(db.collection("Users").document(testUsername).collection("items")).thenReturn(itemsRef);
         Mockito.when(db.collection("Users").document(testUsername).collection("tags")).thenReturn(tagsRef);
 
-        dbController = new DatabaseController(testUsername, db, sb);
+        dbController = new DatabaseController(testUsername, db, sb, itemDataList, tagDataList);
     }
 
     @Test
@@ -349,7 +355,91 @@ public class FireBaseControllerTest {
     public void getItemByDocumentTest() {
         String documentId = "docId";
         ItemCallback callback = mock(ItemCallback.class);
+        DocumentReference docRef = mock(DocumentReference.class, Mockito.RETURNS_DEEP_STUBS);
 
+        dbController.getItem(documentId, callback);
+
+        when(itemsRef.document(documentId)).thenReturn(docRef);
+        when(docRef.get().addOnSuccessListener(documentSnapshot -> {}).addOnFailureListener(any())).thenReturn(null);
+
+        verify(docRef).get();
 
     }
+
+    @Test
+    public void removeTagTest() {
+        Tag tag = new Tag("tag1");
+        tag.setItems(new ArrayList<>(Arrays.asList("item1", "item2")));
+        tagDataList.add(tag);
+        int i = 0;
+        TagModifyCallback callback = mock(TagModifyCallback.class, Mockito.RETURNS_DEEP_STUBS);
+        when(itemsRef.document(anyString()).update(any()).addOnSuccessListener(any()).addOnFailureListener(any())).thenReturn(null);
+
+        dbController.removeTag(i, callback);
+
+        verify(itemsRef.document(anyString()), times(3)).update(any());
+        verify(tagsRef.document("tag1").delete()).addOnSuccessListener(any());
+    }
+
+    @Test
+    public void removeItem() {
+        Item item = mock(Item.class, Mockito.RETURNS_DEEP_STUBS);
+        itemDataList.add(item);
+        when(item.getTags()).thenReturn(new ArrayList<>(Arrays.asList("tag1", "tag2")));
+        when(item.getPhotos()).thenReturn(new ArrayList<>(Arrays.asList("photo1", "photo2")));
+        when(itemsRef.document(item.getDocId()).delete().addOnSuccessListener(any())).thenReturn(null);
+
+        StorageReference photoRef = mock(StorageReference.class, Mockito.RETURNS_DEEP_STUBS);
+        when(imgRef.child(anyString())).thenReturn(photoRef);
+        when(photoRef.delete().addOnSuccessListener(any()).addOnFailureListener(any())).thenReturn(null);
+
+        dbController.removeItem(0);
+
+        verify(photoRef, times(3)).delete();
+        verify(tagsRef.document("tag1")).update(any());
+        verify(tagsRef.document("tag2")).update(any());
+        verify(itemsRef.document(item.getDocId()), times(2)).delete();
+    }
+
+    @Test
+    public void removeTagFromItemTest() {
+        Item item = mock(Item.class, Mockito.RETURNS_DEEP_STUBS);
+        when(item.getDocId()).thenReturn("docId");
+
+        dbController.removeTagfromItem(item, "tag1");
+
+        verify(tagsRef.document("tag1").update(any()).addOnSuccessListener(any())).addOnFailureListener(any());
+    }
+
+    @Test
+    public void removeAllItems() {
+        Item item1 = mock(Item.class);
+        Item item2 = mock(Item.class);
+        Item item3 = mock(Item.class);
+        ArrayList<Integer> toRemove = new ArrayList<>();
+
+        toRemove.add(0);
+        toRemove.add(1);
+        toRemove.add(2);
+
+        itemDataList.add(item1);
+        itemDataList.add(item2);
+        itemDataList.add(item3);
+
+        for (Item item: itemDataList) {
+            when(item.getTags()).thenReturn(new ArrayList<>(Arrays.asList("tag1", "tag2")));
+            when(item.getPhotos()).thenReturn(new ArrayList<>(Collections.singletonList("photo1")));
+        }
+
+        StorageReference photoRef = mock(StorageReference.class, Mockito.RETURNS_DEEP_STUBS);
+
+        when(imgRef.child(anyString())).thenReturn(photoRef);
+        when(photoRef.delete().addOnSuccessListener(any()).addOnFailureListener(any())).thenReturn(null);
+
+        dbController.removeAllItems(toRemove);
+
+        verify(itemsRef.document(any()), times(3)).delete();
+    }
+
+
 }
